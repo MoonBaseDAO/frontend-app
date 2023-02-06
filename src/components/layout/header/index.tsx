2c35745ff844f988ff62dfc4ca2768c81f6dd5bc
devofficer <myeyesontech@gmail.com>
2023-02-06 05:23:01 -0800

    [feat] navbar for the frontend

---
+++ src/components/layout/header/index.tsx
@@ -0,0 +1,29 @@
+import { Navbar, Button, Text } from "@nextui-org/react";
+import Logo from './logo';
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <Navbar maxWidth="fluid" variant="floating">
+      <Navbar.Brand>
+        <Navbar.Toggle showIn="xs" aria-label="toggle navigation" />
+        <Logo />
+        <Text b color="inherit">MoonBase</Text>
+      </Navbar.Brand>
+      <Navbar.Content enableCursorHighlight hideIn="xs" variant="highlight">
+        <Navbar.Link isActive href="#">Home</Navbar.Link>
+        <Navbar.Link href="#">Bounties Area</Navbar.Link>
+        <Navbar.Link href="#">Actions</Navbar.Link>
+      </Navbar.Content>
+      <Navbar.Content>
+        <Navbar.Item>
+          <Button auto flat as={Link} href="#">
+            Connect
+          </Button>
+        </Navbar.Item>
+      </Navbar.Content>
+    </Navbar>
+  );
+}
+
+export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
+import Header from '@/components/layout/header';
 import { NextUIProvider } from '@nextui-org/react';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
+      <Header/>
       <Component {...pageProps}/>
     </NextUIProvider>
   );
