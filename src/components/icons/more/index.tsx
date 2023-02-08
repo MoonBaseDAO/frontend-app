43e5baa9e2f0128f9e0d2f600671d49a39a5d41a
devofficer <myeyesontech@gmail.com>
2023-02-08 07:36:32 -0800

    [feat] schedule card

---
+++ src/components/icons/more/index.tsx
@@ -0,0 +1,29 @@
+import { IconProps } from "../type";
+
+export const MoreIcon = ({
+  fill = 'currentColor',
+  filled,
+  size,
+  height,
+  width,
+  label,
+  ...props
+}: IconProps) => {
+  return (
+    <svg
+      width={size || width || 24}
+      height={size || height || 24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 12c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"
+        fill={fill}
+      />
+    </svg>
+  );
+};
